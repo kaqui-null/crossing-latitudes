3d4459e6a6bdd78c0d3dc9d0c5ed12dc2ca58c67
kaqui-null <emanuelgomes24.08@gmail.com>
2025-06-01 19:43:46 -0300

    added shoot()

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var direction = Vector2.RIGHT
+@export var magic_scene = PackedScene
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -19,3 +20,32 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
+
+func shoot():
+	if magic_scene:
+		var instance = magic_scene.instanciate()
+		get_tree().current_scene.add_child(instance)
+		
+		if Input.is_action_pressed("aim_mode"):
+			var input_dir = Input.get_vector("left", "right", "up", "down")
+			dir = get_aim_direction(input_dir)
+
+
+func get_aim_direction(input_dir: Vector2) -> Vector2:
+	if input_dir.length() == 0:
+		return facing_direction  # ou qualquer padrão
+	
+	var angle = rad_to_deg(input_dir.angle())
+	
+	if angle < -157.5 or angle > 157.5:
+		return Vector2.LEFT
+	elif angle < -112.5:
+		return (Vector2.LEFT + Vector2.UP).normalized()
+	elif angle < -67.5:
+		return Vector2.UP
+	elif angle < -22.5:
+		return (Vector2.RIGHT + Vector2.UP).normalized()
+	elif angle < 22.5:
+		return Vector2.RIGHT
+
+	return facing_direction
